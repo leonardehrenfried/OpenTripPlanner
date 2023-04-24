6eed5823b4faab950a89e78a1cf310909912a98e
Leonard Ehrenfried <mail@leonard.io>
2023-04-24 16:47:47 +0200

    Use parameterized tests

--- src/test/java/org/opentripplanner/graph_builder/module/osm/WalkableAreaBuilderTest.java
+++ src/test/java/org/opentripplanner/graph_builder/module/osm/WalkableAreaBuilderTest.java
@@ -1,41 +1,35 @@
 package org.opentripplanner.graph_builder.module.osm;
 
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.util.stream.Collectors.toMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.File;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.opentripplanner.graph_builder.issue.api.DataImportIssueStore;
 import org.opentripplanner.graph_builder.services.osm.CustomNamer;
 import org.opentripplanner.openstreetmap.OpenStreetMapProvider;
 import org.opentripplanner.openstreetmap.model.OSMLevel;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.street.model.edge.AreaEdge;
+import org.opentripplanner.test.support.VariableSource;
 import org.opentripplanner.transit.model.framework.Deduplicator;
 
 public class WalkableAreaBuilderTest {
 
+  private static final String LUND_STATION_OSM = "lund-station-sweden.osm.pbf";
+  private static final int NODE_ID_ON_PLATFORM_6 = 1025307935;
   private final Deduplicator deduplicator = new Deduplicator();
-  private final Graph graph = new Graph(deduplicator);
 
-  @BeforeEach
-  public void testSetup(final TestInfo testInfo) {
-    final Method testMethod = testInfo.getTestMethod().get();
-    final String osmFile = testMethod.getAnnotation(OsmFile.class).value();
-    final boolean visibility = testMethod.getAnnotation(Visibility.class).value();
+  public Graph buildGraph(String osmFileName, boolean visibility) {
+    final Graph graph = new Graph(deduplicator);
     final boolean platformEntriesLinking = true;
     final int maxAreaNodes = 5;
 
@@ -59,7 +53,7 @@ public class WalkableAreaBuilderTest {
       new HashMap<>()
     );
 
-    final File file = new File(testInfo.getTestClass().get().getResource(osmFile).getFile());
+    final File file = new File(this.getClass().getResource(osmFileName).getFile());
     new OpenStreetMapProvider(file, true).readOSM(osmdb);
     osmdb.postLoad();
 
@@ -80,55 +74,32 @@ public class WalkableAreaBuilderTest {
     final List<AreaGroup> areaGroups = AreaGroup.groupAreas(areasLevels);
 
     final Consumer<AreaGroup> build = visibility
-      ? ag -> walkableAreaBuilder.buildWithVisibility(ag)
-      : ag -> walkableAreaBuilder.buildWithoutVisibility(ag);
+      ? walkableAreaBuilder::buildWithVisibility
+      : walkableAreaBuilder::buildWithoutVisibility;
 
     areaGroups.forEach(build);
+    return graph;
   }
 
-  // -- Tests --
+  static Stream<Arguments> areaCases = Stream.of(
+    Arguments.of(LUND_STATION_OSM, true, NODE_ID_ON_PLATFORM_6),
+    Arguments.of(LUND_STATION_OSM, false, NODE_ID_ON_PLATFORM_6)
+  );
 
-  @Test
-  @OsmFile("lund-station-sweden.osm.pbf")
-  @Visibility(true)
-  public void test_calculate_vertices_area() {
+  @ParameterizedTest(
+    name = "should add areas on osm way containing node {2} from file {0} when visibility={1} "
+  )
+  @VariableSource("areaCases")
+  public void testAreaCreation(String filename, boolean visibility, int nodeId) {
+    var graph = buildGraph(filename, visibility);
     var areas = graph
       .getEdgesOfType(AreaEdge.class)
       .stream()
-      .filter(a -> a.getToVertex().getLabel().equals("osm:node:1025307935"))
+      .filter(a -> a.getToVertex().getLabel().equals("osm:node:" + nodeId))
       .map(AreaEdge::getArea)
       .distinct()
       .toList();
     assertEquals(1, areas.size());
     assertFalse(areas.get(0).getAreas().isEmpty());
-  }
-
-  @Test
-  @OsmFile("lund-station-sweden.osm.pbf")
-  @Visibility(false)
-  public void testSetup_calculate_vertices_area_without_visibility() {
-    var areas = graph
-      .getEdgesOfType(AreaEdge.class)
-      .stream()
-      .filter(a -> a.getToVertex().getLabel().equals("osm:node:1025307935"))
-      .map(AreaEdge::getArea)
-      .distinct()
-      .toList();
-    assertEquals(1, areas.size());
-    assertFalse(areas.get(0).getAreas().isEmpty());
-  }
-
-  // -- Infrastructure --
-
-  @Retention(RUNTIME)
-  @Target(ElementType.METHOD)
-  public @interface OsmFile {
-    String value();
-  }
-
-  @Retention(RUNTIME)
-  @Target(ElementType.METHOD)
-  public @interface Visibility {
-    boolean value();
   }
 }
